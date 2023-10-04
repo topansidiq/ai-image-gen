acd7d1dceaa540329a5e8e1430ff8d41677bfb73
unknown <you@example.com>
2023-10-04 10:21:06 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 2807;
+
+const app = express();
+
+// Enable body paser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/openai", require("./routes/openaiRoutes"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
